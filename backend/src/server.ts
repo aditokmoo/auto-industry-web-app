2554dc1484975ffe942a19e6005aed40360afd9c
Havock48 <adit.github@gmail.com>
2024-09-16 15:14:10 +0200

    backend initial commit

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+import app from './app';
+
+
+const PORT = process.env.PORT || 4000;
+const DB = process.env.MONGO_DB_URL?.replace('<db_password>', process.env.MONGO_DB_PASSWORD!);
+
+mongoose.connect(DB!).then(() => console.log('Successfully connected to DB')).catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`)
+})
