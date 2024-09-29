6f7e96ef6e5c8e459762631556c4a5c588f65508
Havock48 <adit.github@gmail.com>
2024-09-29 13:53:05 +0200

    backend - filtering added

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -2,16 +2,27 @@ import asyncHandler from "express-async-handler";
 import User from "../models/User";
 
 export const getUsers = asyncHandler(async (req, res) => {
-    const { type } = req.query;
+    const { type, groups } = req.query;
 
-    const allowedRoles = ["customer", 'serviceProvider'];
+    const allowedRoles = ["customer", "serviceProvider"];
 
-    if(!allowedRoles.includes(type as string)) {
-        res.status(400).json({ status: 'error', message: 'Invalid role type. Only "customer" and "serviceProvider" are allowed ' })
-        return
+    if (!allowedRoles.includes(type as string)) {
+        res.status(400).json({ status: "error", message: 'Invalid role type. Only "customer" and "serviceProvider" are allowed' });
+        return;
     }
 
-    const users = await User.find({ role: type }, 'name email id phoneNumber workImages profileImage serviceProviderAppointments group location');
+    let groupArray: string[] = [];
 
-    res.status(200).json({ status: 'success', users });
+    if (Array.isArray(groups)) {
+        groupArray = groups.map(group => String(group));
+    } else if (typeof groups === "string") {
+        groupArray = groups.split(",").map(group => group.trim());
+    }
+
+    const users = await User.find({
+        role: type,
+        ...(groupArray.length > 0 && { group: { $in: groupArray } })
+    }, 'name email id phoneNumber workImages profileImage serviceProviderAppointments group location');
+
+    res.status(200).json({ status: "success", users });
 });
